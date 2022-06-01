8366e30685cc906fcec504d09113a59d1d97886e
Jesse Bye <jessesbye@gmail.com>
2022-06-01 08:24:14 -0700

    remove ecr public

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { run } from './run'
 
 const main = async (): Promise<void> => {
   await run({
-    public: core.getBooleanInput('public', { required: true }),
     repository: core.getInput('repository', { required: true }),
     lifecyclePolicy: core.getInput('lifecycle-policy'),
   })
--- src/run.ts
+++ src/run.ts
@@ -1,23 +1,12 @@
 import * as core from '@actions/core'
 import { runForECR } from './ecr'
-import { runForECRPublic } from './ecr_public'
 
 interface Inputs {
-  public: boolean
   repository: string
   lifecyclePolicy: string
 }
 
 export const run = async (inputs: Inputs): Promise<void> => {
-  if (inputs.public === true) {
-    if (inputs.lifecyclePolicy) {
-      throw new Error(`currently ECR Public does not support the lifecycle policy`)
-    }
-    const outputs = await runForECRPublic(inputs)
-    core.setOutput('repository-uri', outputs.repositoryUri)
-    return
-  }
-
   const outputs = await runForECR({
     repository: inputs.repository,
     lifecyclePolicy: inputs.lifecyclePolicy !== '' ? inputs.lifecyclePolicy : undefined,
