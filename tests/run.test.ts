bad37a7243a6144e48e5927a809293d42d42f837
Jesse Bye <jbye@riskalyze.com>
2022-06-01 08:50:00 -0700

    bumps

--- tests/run.test.ts
+++ tests/run.test.ts
@@ -1,6 +1,5 @@
 import { mockClient } from 'aws-sdk-client-mock'
 import * as ecr from '@aws-sdk/client-ecr'
-import * as ecrPublic from '@aws-sdk/client-ecr-public'
 import * as core from '@actions/core'
 import { run } from '../src/run'
 
@@ -15,7 +14,6 @@ const setOutputMock = core.setOutput as jest.Mock
 
 const mocks = {
   ecr: mockClient(ecr.ECRClient),
-  ecrPublic: mockClient(ecrPublic.ECRPUBLICClient),
 }
 
 test('ecr', async () => {
@@ -33,24 +31,6 @@ test('ecr', async () => {
   await run({
     repository: 'foo/bar',
     lifecyclePolicy: `${__dirname}/fixtures/lifecycle-policy.json`,
-    public: false,
   })
   expect(setOutputMock).toBeCalledWith('repository-uri', '123456789012.dkr.ecr.ap-northeast-1.amazonaws.com/foobar')
-})
-
-test('ecr public', async () => {
-  mocks.ecrPublic.on(ecrPublic.DescribeRepositoriesCommand).resolves({
-    repositories: [
-      {
-        repositoryName: 'foobar',
-        repositoryUri: 'public.ecr.aws/12345678/foobar',
-      },
-    ],
-  })
-  await run({
-    repository: 'foo/bar',
-    lifecyclePolicy: '',
-    public: true,
-  })
-  expect(setOutputMock).toBeCalledWith('repository-uri', 'public.ecr.aws/12345678/foobar')
 })
